b6623efe4e141b9fd43353172d6d476e33dc3af3
agate-pris <agate-pris@users.noreply.github.com>
2021-10-18 23:35:14 +0900

    Add fontcustom

---
+++ fontcustom/build.sh
@@ -0,0 +1,4 @@
+#!/#bin/sh
+fontcustom compile
+cp fontcustom/fontcustom.css ../content/css/
+cp fontcustom/fontcustom.svg  fontcustom/fontcustom.ttf  fontcustom/fontcustom.woff  fontcustom/fontcustom.woff2 ../content/fonts/
